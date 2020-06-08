4377b0a9c3698c4ed008bbb3ea45a755e94422b8
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 03:47:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+
+function happyHolidays() {
+  return console.log("Happy holidays!");
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, " + name"!";
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return "";
+}
